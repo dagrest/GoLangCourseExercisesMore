194f159cc7ef218944dc5ac1b0f371180c468ba5
David Agrest <david.agrest@snyk.io>
2022-09-26 11:54:53 +0300

    maps

---
+++ section41_DataStructures_Map/map/map.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type Person struct {
+	first string
+	last  string
+	age   int
+}
+
+func main() {
+	fmt.Println("Map Examples")
+
+	p0 := Person{"James", "Bond", 37}
+	p1 := Person{"Monney", "Penny", 25}
+
+	fmt.Println(p0, p1)
+
+	m := make(map[string]Person)
+
+	m["007"] = p0
+	m["mp"] = p1
+
+	for k, v := range m {
+		fmt.Println(k, v)
+	}
+	fmt.Println("Map length: ", len(m))
+
+	value, exists := m["007"]
+	fmt.Println("Value 007: ", value, exists)
+
+	value, exists = m["001"]
+	fmt.Println("Value 001: ", value, exists)
+}
